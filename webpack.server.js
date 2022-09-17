53b6b6afd3ef855c8c64520bd89d4fabd6a1cd63
vdokkupalle-ebsco <75068341+vdokkupalle-ebsco@users.noreply.github.com>
2022-09-17 23:09:47 +0530

    added webpack merge

--- webpack.server.js
+++ webpack.server.js
@@ -1,26 +1,14 @@
 const path = require("path");
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
 
-module.exports = {
+const config = {
   target: "node",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            ["env", { targets: { browsers: ["last 2 versions"] } }],
-          ],
-        },
-      },
-    ],
-  },
 };
+
+module.exports = merge(baseConfig, config);
