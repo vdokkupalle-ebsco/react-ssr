64f38fe555e7fc75fa702d430a06628d4bb35353
vdokkupalle-ebsco <75068341+vdokkupalle-ebsco@users.noreply.github.com>
2022-09-17 22:50:02 +0530

    change render to hydrate

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "react-dom";
+import { hydrate } from "react-dom";
 
 import Home from "./components/Home";
 
-render(<Home />, document.querySelector("#root"));
+hydrate(<Home />, document.querySelector("#root"));
